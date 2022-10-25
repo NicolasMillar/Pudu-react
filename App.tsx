ead6f179077591a80b44b1ea7a05bacdab9604ed
NicolasMillar <nicolas.millar1601@alumnos.ubiobio.cl>
2022-10-24 22:22:15 -0300

    Prueba 2

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Prueba nuevo</Text>
+      <Text>Prueba nuevo asdasd</Text>
       <StatusBar style="auto" />
     </View>
   );
